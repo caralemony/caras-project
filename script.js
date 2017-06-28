e40b35a585c81dd9e8e364fcaedea32c5b566c9d
Caroline <carolinelappleby@gmail.com>
2017-06-28 20:50:36 +0100

    added changing time

--- script.js
+++ script.js
@@ -2,5 +2,17 @@
 var now = new Date();
 var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var day = document.getElementById("day");
+var time = document.getElementById("time");
 var theDay = weekDays[now.getDay()];
+
+
+function setTime() {
+  var now = new Date();
+  var clock = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+  time.innerHTML = clock;
+
+}
+
 day.innerHTML= theDay;
+setTime();
+setInterval(setTime, 1000);
