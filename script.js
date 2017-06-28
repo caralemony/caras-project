90d8906fc6d3c09d2557ce637abccc2dcaf915b5
Caroline <carolinelappleby@gmail.com>
2017-06-28 21:07:25 +0100

    hgugflh

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-
-var now = new Date();
 var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var day = document.getElementById("day");
 var time = document.getElementById("time");
-var theDay = weekDays[now.getDay()];
+
 
 
 function setTime() {
   var now = new Date();
+  var theDay = weekDays[now.getDay()];
   var clock = padZeros(now.getHours()) + ":" + padZeros(now.getMinutes()) + ":" + padZeros(now.getSeconds());
   time.innerHTML = clock;
+  day.innerHTML= theDay;
 
 }
 
@@ -20,6 +20,6 @@ function padZeros(num) {
 return num;
 }
 
-day.innerHTML= theDay;
+
 setTime();
 setInterval(setTime, 1000);
